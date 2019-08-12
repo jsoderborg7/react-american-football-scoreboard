fbc8e88949aeb858d58f6f4ad150ea1fb86bac52
Jenn Soderborg <jsoderborg7@gmail.com>
2019-08-12 14:17:00 -0600

    turning the scoreboard into a hockey board

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 //TODO: STEP 1 - Import the useState hook.
 import React, {useState} from "react";
 import "./App.css";
-import BottomRow from "./BottomRow";
+import BottomRow from "./HockeyBottomRow";
+
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
 
     const [homeScore, setHomeScore] = useState(0);
     const [awayScore, setAwayScore] = useState(0);
+    const [period, setPeriod]= useState(1);
 
   return (
     <div className="container">
@@ -20,7 +22,13 @@ function App() {
 
             <div className="home__score">{homeScore}</div>
           </div>
-          <div className="timer">00:03</div>
+          <div className="topMiddle">
+            <div className="timer">00:03</div>
+            <div className="period">
+              <h3 className="period__title">Period</h3>
+              <div className="period__value">{period}</div>
+            </div>
+          </div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
             <div className="away__score">{awayScore}</div>
